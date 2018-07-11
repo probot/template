dcb7fe4d68d9a07bc32f7d13f557b90386199d02
Wilhelm Klopp <wil.klopp@gmail.com>
2018-07-11 18:06:50 +0800

    Add app.on "Hello World"

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
